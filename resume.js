61632f0601d0a3240833d584baa77d14b87d9d08
SARATHKUMAR-T <123524854+SARATHKUMAR-T@users.noreply.github.com>
2023-02-23 12:43:09 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,19 @@
+var resume={
+    "name":"SARATH KUMAR T",
+    "DOB":"01.11.1998",
+    "Mail id":"sarathkumartk98@gmail.com",
+    "contact no":"6383406054",
+    "Education":"goverment engineering college",
+    "year of passing":"2022",
+    "CGPA":"8.6",
+    "Department":"Automobile",
+    "coursework":"Strength of materials,mechanics of machines,design of machine elements,spark ignition engines,compression ignition engines",
+    "skill":"SKILFUL IN PEOPLE MANAGEMENT,ENGINE DISMANTLING,OBD2",
+    "projects":"Nox reduction using ammonia injection",
+    "Nox reduction using ammonia injection":"Considering the modern emission laws, reducing NOx in CI engines is a major concern. We make a Standalone Ammonia Injection system for Old vehicles that are working in BS3 and BS4 emission standards"
+
+
+    
+
+}
+console.log(resume);
