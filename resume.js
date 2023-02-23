a7279bc3e059d580b44bffafd2e15659cb34fd08
SARATHKUMAR-T <123524854+SARATHKUMAR-T@users.noreply.github.com>
2023-02-23 12:48:47 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,30 @@
+
+//1. diffrence between window,screen and object ?
+
+//window is the main JavaScript object root, aka the global object in a browser, and it can also be treated as the root of the document object model. You can access it as window.
+
+//window.screen or just screen is a small information object about physical screen dimensions.
+
+//window.document or just document is the main object of the potentially visible (or better yet: rendered) document object model/DOM.
+
+
+
+var resume={
+    "name":"SARATH KUMAR T",
+    "DOB":"01.11.1998",
+    "Mail id":"sarathkumartk98@gmail.com",
+    "contact no":"6383406054",
+    "Education":"goverment engineering college",
+    "year of passing":"2022",
+    "CGPA":"8.6",
+    "Department":"Automobile",
+    "coursework":"Strength of materials,mechanics of machines,design of machine elements,spark ignition engines,compression ignition engines",
+    "skill":"SKILFUL IN PEOPLE MANAGEMENT,ENGINE DISMANTLING,OBD2",
+    "projects":"Nox reduction using ammonia injection",
+    "Nox reduction using ammonia injection":"Considering the modern emission laws, reducing NOx in CI engines is a major concern. We make a Standalone Ammonia Injection system for Old vehicles that are working in BS3 and BS4 emission standards"
+
+
+    
+
+}
+console.log(resume);
